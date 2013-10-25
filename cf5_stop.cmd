3e28db22002f17c293a52ae4f8ef2a1055516916
Al Serize <al.serize@esmartgroup.co>
2013-10-25 09:56:56 -0400

    Stop Coldfusion 9 Services
    Stop Coldfusion 9 Services from a batch process file for Windows

---
+++ cf5_stop.cmd
@@ -0,0 +1,14 @@
+net stop "ColdFusion Monitoring Service"
+net stop "ColdFusion Management Service"
+net stop "ColdFusion Management Repository Server"
+net stop "ColdFusion Graphing Server"
+net stop "ColdFusion RDS"
+net stop "Cold Fusion Application Server"
+net stop "ColdFusion Executive"
+net stop "Siteminder Authorization Service"
+net stop "SiteMinder Authentication Service"
+net stop "ColdFusion MX Application Server"
+net stop "ColdFusion MX ODBC Agent"
+net stop "ColdFusion MX ODBC Server"
+
+pause
